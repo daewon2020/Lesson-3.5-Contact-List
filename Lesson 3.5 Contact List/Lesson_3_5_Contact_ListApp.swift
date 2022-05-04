db847ba2c7b0bae6afbf27e53b7d7daa67ced826
Kostya Andreev <daewon@yandex.ru>
2022-05-04 21:10:19 +0300

    Initial Commit

---
+++ Lesson 3.5 Contact List/Lesson_3_5_Contact_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_3_5_Contact_ListApp.swift
+//  Lesson 3.5 Contact List
+//
+//  Created by Константин Андреев on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_3_5_Contact_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
